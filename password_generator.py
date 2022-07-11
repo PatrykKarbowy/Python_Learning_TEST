dfa909e0272cd03af0f50c82b521c080e6f1bad2
Patryk Karbowy <patryk.karbowy11@gmail.com>
2022-07-11 17:12:53 +0200

    Commit

---
+++ password_generator.py
@@ -0,0 +1,41 @@
+
+SYMBOLS = {'~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/'}
+is_strong = False
+
+
+def password_symbol_counter (password):
+    symbol_number = 0
+    for ch in password:
+        if ch in SYMBOLS:
+            symbol_number += 1
+    return symbol_number
+
+
+def password_upper_case_counter (password):
+    upper_case_number = 0
+    for ch in password:
+        if ch.isupper():
+            upper_case_number += 1
+    return upper_case_number
+
+
+def password_strenght(symbol, upper_case):
+    if symbol > 2 and upper_case > 1:
+        print("Your Password is very strong!")
+    elif symbol > 1 and upper_case > 1:
+        print("Your password is medium strong")
+        global is_strong
+        is_strong = True
+        return is_strong
+    else:
+        print("Your password is not strong enought...")
+
+
+while is_strong == False:
+    password = input("Type your new password: \n")
+    if len(password) < 9:
+        print("Password should be at least 8 characters long")
+    else:
+        symbol_number = password_symbol_counter(password)
+        upper_case_number = password_upper_case_counter(password)
+        password_strenght(symbol_number, upper_case_number)
