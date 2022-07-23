c2d7fa719c10650c9a43af8a7c93dc8597f0f816
Patryk Karbowy <patryk.karbowy11@gmail.com>
2022-07-23 23:49:11 +0200

    Finishing Project

--- SNAKE_PYGAME/snake.py
+++ SNAKE_PYGAME/snake.py
@@ -15,12 +15,13 @@ ORANGE = (255,165,0)
 GREY = (64,64,64)
 
 # SNAKE SIZE
-SIZE = 40
+SIZE = 80
 
 # GRID
 ROWS = WIDTH // SIZE
 COLUMNS = WIDTH // SIZE
 
+# SNAKE CLASS
 class Snake():
     def __init__(self, x, y):
         self.x = x
@@ -31,6 +32,8 @@ class Snake():
         self.tail = tail
 
     def move(self):
+
+        # PERFORMING MOVE ACTIONS ACCORDING TO CLICKED BUTTON
         keys = pygame.key.get_pressed()
         if self.x_speed == 0:
             if keys[pygame.K_RIGHT]:
@@ -47,22 +50,29 @@ class Snake():
                 self.x_speed = 0
                 self.y_speed = 1
 
+        # MOVING SNAKE
         self.tail.insert(0, Segment(self.x, self.y))
         self.x += self.x_speed
         self.y += self.y_speed
 
+        # CHECKING IF SNAKE IS INSIDE WINDOW
         if self.x < 0 or self.x == COLUMNS or self.y < 0 or self. y == ROWS:
             pygame.quit()
 
+        # CHECKING IF SNAKE HEAD TOUCHED IT'S TAIL
         for segment in self.tail[1:]:
             if self.x == segment.x and self.y == segment.y:
                 pygame.quit()
 
+        # GENERATING NEW FOOD
         if self.x == food.x and self.y == food.y:
             while True:
                 food.x = randrange(0,COLUMNS)
                 food.y = randrange(0,ROWS)
-                break
+                if len(list(filter(lambda segment: segment.x == food.x and segment.y == food.y, self.tail))) > 0:
+                    continue
+                else:
+                    break
         else:
             self.tail.pop()
 
@@ -87,9 +97,11 @@ class Food():
     def draw(self):
         pygame.draw.rect(WIN, RED, (self.x * SIZE, self.y * SIZE, SIZE, SIZE))
 
+# SETTING UP GAME OBJECTS
 snake  = Snake(COLUMNS // 2, ROWS // 2)
 food = Food(randrange(0,COLUMNS),randrange(0,ROWS))
 
+# SCREEN RENDER
 def render_screen():
     WIN.fill(BLACK)
     draw_grid()
@@ -97,11 +109,13 @@ def render_screen():
     food.draw()
     pygame.display.update()
 
+# FUNCTION THAT DRAWS GRID
 def draw_grid():
     for row in range(ROWS):
         pygame.draw.line(WIN, GREY, (row*SIZE - 1, 0), (row*SIZE - 1, WIDTH), 2)
         pygame.draw.line(WIN, GREY, (0, row*SIZE - 1), (WIDTH, row*SIZE - 1), 2)
 
+#   MAIN GAME LOOP
 def main():
     clock = pygame.time.Clock()
     while True:
