0a2e416925d8b96933316bb813770000cfc01f15
Patryk Karbowy <patryk.karbowy11@gmail.com>
2022-07-28 18:50:50 +0200

    Updating

--- FLASK/app.py
+++ FLASK/app.py
@@ -25,6 +25,22 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # Update Item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # Delete item
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
 if __name__ == "__main__":
     db.create_all()
     
