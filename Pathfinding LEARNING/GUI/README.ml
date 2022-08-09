96323469f2439b2df6600924947ed78e8ee2bb0b
Patryk Karbowy <57367362+PatrykKarbowy@users.noreply.github.com>
2022-08-09 17:21:52 +0200

    Update README.ml

--- Pathfinding LEARNING/GUI/README.ml
+++ Pathfinding LEARNING/GUI/README.ml
@@ -1,1 +1,31 @@
-HERE I POST SOME PATHFINDING VISUALIZERS IN PYGAME
+
+# PyGame Pathfinding algorithms visualizers
+
+This is my pathfinding algorithm visualizers (including Astar and Djikstra). Whole coded in order to get better with classes and python language.
+
+
+
+
+
+## Demo
+
+![](https://github.com/PatrykKarbowy/Python_Learning_TEST/tree/master/SNAKE_PYGAME/Snake_DEMO.gif)
+
+
+## Installation
+
+Install Pathfinding visualizers
+
+```bash
+  pip install pygame
+  python Astar.py
+  python Djikstra.py
+```
+    
+## Instructions
+
+- LEFT_MOUSE_BUTTON -> Adding start node
+- RIGHT_MOUSE_BUTTON -> Adding end node
+- MIDDLE_MOUSE_BUTTON -> Adding barriers
+- DELETE_KEY with mouse hover -> Removing node
+
